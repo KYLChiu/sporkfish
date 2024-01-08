e3934882de724ccfe53c686c2a4c74a22145d916
Kelvin C <kchiu1997@gmail.com>
2024-01-08 22:53:26 +0000

    Skip slow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,26 @@
 import sys
 import os
+import pytest
 
 # Add the parent directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
--- tests/test_bratko_kopec.py
+++ tests/test_bratko_kopec.py
@@ -39,8 +39,9 @@ def analyser(epd: str):
     return 0.0
 
 
-# def test_bratko_kopec():
-#     score = 0.0
-#     for epd in epds.split("\n"):
-#         score += analyser(epd)
-#     print(score)
+@pytest.mark.slow
+def test_bratko_kopec():
+    score = 0.0
+    for epd in epds.split("\n"):
+        score += analyser(epd)
+    print(score)
