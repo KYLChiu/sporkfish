8ecb5750efe8e323f105f1149f2b3bc3717d407e
Kelvin C <kchiu1997@gmail.com>
2024-01-16 08:43:47 +0000

    Zobrist hashing improvement part1

--- sporkfish/searcher.py
+++ sporkfish/searcher.py
@@ -13,7 +13,7 @@ from .configurable import Configurable
 from .evaluator import Evaluator
 from .statistics import Statistics
 from .transposition_table import TranspositionTable
-from .zobrist_hash import ZobristHash
+from .zobrist_hasher import ZobristHasher
 from enum import Enum
 
 
@@ -48,7 +48,7 @@ class SearcherConfig(Configurable):
         self,
         max_depth: int = 5,
         mode: SearchMode = SearchMode.SINGLE_PROCESS,
-        enable_transposition_table: bool = True,
+        enable_transposition_table: bool = False,
     ) -> None:
         self.max_depth = max_depth
         # TODO: register the constructor function in yaml loader instead.
@@ -79,12 +79,13 @@ class Searcher:
 
         self._evaluator = evaluator
         self._config = config
-        self._zobrist_hash = ZobristHash()
+
         self._statistics = Statistics(_stats)
 
         if self._config.enable_transposition_table:
-            logging.info("Enabled transposition table in search.")
+            self._zobrist_hash = ZobristHasher()
             self._transposition_table = TranspositionTable(_dict)
+            logging.info("Enabled transposition table in search.")
         else:
             logging.info("Disabled transposition table in search.")
 
