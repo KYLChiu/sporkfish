0ac4cc52506ac5f8106dd925572792141094a0fc
Kelvin C <kchiu1997@gmail.com>
2024-01-17 19:55:39 +0000

    Fix argument ordering

--- tests/test_searcher.py
+++ tests/test_searcher.py
@@ -49,22 +49,22 @@ def run_perft(
     stats.strip_dirs().sort_stats("tottime").print_stats(10)
 
 
-# Performance test without transposition table and with null-move pruning
-def test_nmp_perf():
-    run_perft(
-        fen="r1r3k1/1ppp1ppp/p7/8/1P1nPPn1/3B1RP1/P1PP3q/R1BQ2K1 w - - 2 18",
-        max_depth=6,
-        enable_null_move_pruning=True,
-        enable_transposition_table=False,
-    )
-
-
 # Performance test without transposition table and without null-move pruning
 def test_perf():
     run_perft(
         fen="r1r3k1/1ppp1ppp/p7/8/1P1nPPn1/3B1RP1/P1PP3q/R1BQ2K1 w - - 2 18",
         max_depth=6,
         enable_null_move_pruning=False,
+        enable_transposition_table=False,
+    )
+
+
+# Performance test without transposition table and with null-move pruning
+def test_nmp_perf():
+    run_perft(
+        fen="r1r3k1/1ppp1ppp/p7/8/1P1nPPn1/3B1RP1/P1PP3q/R1BQ2K1 w - - 2 18",
+        max_depth=6,
+        enable_null_move_pruning=True,
         enable_transposition_table=False,
     )
 
