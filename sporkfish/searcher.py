b43ad9efa9bace4f3f059488e749a56a24ee0e12
Kelvin C <kchiu1997@gmail.com>
2024-01-17 19:52:06 +0000

    Test perf

--- sporkfish/searcher.py
+++ sporkfish/searcher.py
@@ -40,6 +40,8 @@ class SearcherConfig(Configurable):
     :type max_depth: int
     :param mode: Search mode (default: SearchMode.SINGLE_PROCESS).
     :type mode: SearchMode
+    :param enable_null_move_pruning: Enable null-move pruning (default: True).
+    :type enable_null_move_pruning: bool
     :param enable_transposition_table: Enable transposition table (default: True).
     :type enable_transposition_table: bool
     """
@@ -48,14 +50,14 @@ class SearcherConfig(Configurable):
         self,
         max_depth: int = 5,
         mode: SearchMode = SearchMode.SINGLE_PROCESS,
-        enable_transposition_table: bool = True,
         enable_null_move_pruning: bool = True,
+        enable_transposition_table: bool = False,
     ) -> None:
         self.max_depth = max_depth
         # TODO: register the constructor function in yaml loader instead.
         self.mode = mode if isinstance(mode, SearchMode) else SearchMode(mode)
-        self.enable_transposition_table = enable_transposition_table
         self.enable_null_move_pruning = enable_null_move_pruning
+        self.enable_transposition_table = enable_transposition_table
 
 
 class Searcher:
