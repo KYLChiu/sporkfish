4243c32df03957144f9e8f6293cfdcb24fa2a809
Kelvin C <kchiu1997@gmail.com>
2024-02-04 12:59:01 +0000

    fix test

--- tests/test_endgame_tablebase.py
+++ tests/test_endgame_tablebase.py
@@ -6,7 +6,7 @@ from perf_helper import run_perf_analytics
 from sporkfish.endgame_tablebase import EndgameTablebase, EndgameTablebaseConfig
 
 
-def et_query(fen: str):
+def move_from_et_query(fen: str):
     board = chess.Board()
     board.set_fen(fen)
     et = EndgameTablebase(EndgameTablebaseConfig("data/endgame_tablebases"))
@@ -14,23 +14,22 @@ def et_query(fen: str):
     return move
 
 
-def et_query_expected(fen: str, move_expected: bool):
-    move = et_query(fen)
-    if move_expected:
-        assert move, "Expected move but none returned."
-    else:
-        assert not move, "Didn't expect move but returned valid move."
-
-
 class TestEndgameTablebase:
+    def _check_et_query_move_expected(self, fen: str, move_expected: bool):
+        move = move_from_et_query(fen)
+        if move_expected:
+            assert move, "Expected move but none returned."
+        else:
+            assert not move, "Didn't expect move but returned valid move."
+
     def test_et_query_white_to_move_white_winning(self):
-        et_query("8/4k3/8/8/8/8/3BB3/3K4 w - - 0 1", True)
+        self._check_et_query_move_expected("8/4k3/8/8/8/8/3BB3/3K4 w - - 0 1", True)
 
     def test_et_query_black_to_move_white_winning(self):
-        et_query("8/4k3/8/8/8/8/3BB3/3K4 b - - 0 1", False)
+        self._check_et_query_move_expected("8/4k3/8/8/8/8/3BB3/3K4 b - - 0 1", False)
 
     def test_et_query_black_to_move_white_winning_extra_queen(self):
-        et_query("8/4k3/8/8/8/8/3BB3/3K1Q2 b - - 0 1", False)
+        self._check_et_query_move_expected("8/4k3/8/8/8/8/3BB3/3K1Q2 b - - 0 1", False)
 
 
 @pytest.mark.parametrize(
@@ -50,4 +49,4 @@ class TestEndgameTablebasePerformance:
         return request
 
     def test_et_query_perf(self, request_fixture, fen_string):
-        run_perf_analytics(request_fixture.node.name, et_query, fen_string)
+        run_perf_analytics(request_fixture.node.name, move_from_et_query, fen_string)
