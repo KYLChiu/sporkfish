bb7bfbde3136fa5c479ab3e65370f036d83daf7a
Kelvin C <kchiu1997@gmail.com>
2024-02-05 11:56:56 +0000

    Test commit

--- scripts/container-postCreate.sh
+++ scripts/container-postCreate.sh
@@ -3,7 +3,11 @@
 # Run pip install
 pip install -r requirements.txt
 
+# Hack...?
+git config --global --add safe.directory $(pwd)
+
 # Install pre-commit and pre-push
-pre-commit install --hook-type pre-commit --hook-type pre-push
+pre-commit install --hook-type pre-commit
+pre-commit install --hook-type pre-push
 
 exit 0
