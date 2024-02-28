9cbae8f95125b1bc052547eaccd24fec3aa7686d
kwngggg <137826910+kwngggg@users.noreply.github.com>
2024-02-28 22:20:27 +0000

    initial commit

--- sporkfish/searcher/move_ordering/composite_heuristic.py
+++ sporkfish/searcher/move_ordering/composite_heuristic.py
@@ -2,15 +2,24 @@ from typing import List
 
 import chess
 
+from config import load_config
+
 from ...board.board import Board
+from ...configurable import Configurable
 from .killer_move_heuristic import KillerMoveHeuristic
 from .move_order_heuristic import MoveOrderHeuristic, MoveOrderMode
 from .mvv_lva_heuristic import MvvLvaHeuristic
 
 
+class Move_ordering_config(Configurable):
+    def __init__(self) -> None:
+        super().__init__()
+
+
 class CompositeHeuristic(MvvLvaHeuristic, KillerMoveHeuristic, MoveOrderHeuristic):
     # TODO: To be tuned later
     # TODO: to be configured later on, issue #120
+    # TODO: create new class here to inherit from configurable.py and pass those settings from config.yml to move order weights
     _MOVE_ORDER_WEIGHTS = {
         MoveOrderMode.MVV_LVA: 2.0,
         MoveOrderMode.KILLER_MOVE: 1.0,
@@ -21,10 +30,12 @@ class CompositeHeuristic(MvvLvaHeuristic, KillerMoveHeuristic, MoveOrderHeuristi
         board: Board,
         killer_moves: List[List[chess.Move]],
         depth: int,
+        config: Move_ordering_config = Move_ordering_config(),
     ) -> None:
         MvvLvaHeuristic.__init__(self, board)
         KillerMoveHeuristic.__init__(self, board, killer_moves, depth)
         MoveOrderHeuristic.__init__(self)
+        self._config = config
 
     def evaluate(self, move: chess.Move) -> float:
         """
@@ -37,10 +48,10 @@ class CompositeHeuristic(MvvLvaHeuristic, KillerMoveHeuristic, MoveOrderHeuristi
         """
         # TODO: this is using is_capture twice but lets leave that for later
         # Simple aggregation for now, to be improved.
-        mvv_lva = CompositeHeuristic._MOVE_ORDER_WEIGHTS[
-            MoveOrderMode.MVV_LVA
-        ] * MvvLvaHeuristic.evaluate(self, move)
-        killer_move = CompositeHeuristic._MOVE_ORDER_WEIGHTS[
-            MoveOrderMode.KILLER_MOVE
-        ] * KillerMoveHeuristic.evaluate(self, move)
+        mvv_lva = self._config.from_dict(
+            load_config().get("MoveOrderingConfig")
+        ).mvv_lva * MvvLvaHeuristic.evaluate(self, move)
+        killer_move = self._config.from_dict(
+            load_config().get("MoveOrderingConfig")
+        ).killer_move * KillerMoveHeuristic.evaluate(self, move)
         return mvv_lva + killer_move
