67171e3fcef74ed6ce49e35948ecbcb153895dde
KYLChiu <kchiu1997@gmail.com>
2024-03-30 21:52:03 +0000

    Fix comment

--- sporkfish/zobrist_hasher.py
+++ sporkfish/zobrist_hasher.py
@@ -7,22 +7,21 @@ from numba import njit
 
 from sporkfish.board.board import Board
 
-_int64_min_val = np.iinfo(np.int64).min
-_int64_max_val = np.iinfo(np.int64).max
-_int8_max_val = np.iinfo(np.int8).max
+_INT64_MIN_VAL = np.iinfo(np.int64).min
+_INT64_MAX_VAL = np.iinfo(np.int64).max
+_INT8_MAX_VAL = np.iinfo(np.int8).max
 
 # TODO: we may revisit this in future as people claim this can affect collision chances
-# We set these globally as we want these to be shared across all Zobrist hashers
 np.random.seed(10101010)
-_piece_keys = np.random.randint(
-    _int64_min_val, _int64_max_val, size=(64, 12), dtype=np.int64
+_PIECE_KEYS = np.random.randint(
+    _INT64_MIN_VAL, _INT64_MAX_VAL, size=(64, 12), dtype=np.int64
 )
-_turn_key = np.random.randint(_int64_min_val, _int64_max_val, dtype=np.int64)
-_en_passant_keys = np.random.randint(
-    _int64_min_val, _int64_max_val, size=8, dtype=np.int64
+_TURN_KEY = np.random.randint(_INT64_MIN_VAL, _INT64_MAX_VAL, dtype=np.int64)
+_EN_PASSANT_KEYS = np.random.randint(
+    _INT64_MIN_VAL, _INT64_MAX_VAL, size=8, dtype=np.int64
 )
-_castling_keys = np.random.randint(
-    _int64_min_val, _int64_max_val, size=16, dtype=np.int64
+_CASTLING_KEYS = np.random.randint(
+    _INT64_MIN_VAL, _INT64_MAX_VAL, size=16, dtype=np.int64
 )
 
 
@@ -36,25 +35,25 @@ def _aggregate_piece_hash(
     ), f"Expected the same number of squares and colored_piece_types but got length {num_pieces}, {len(colored_piece_types)} respectively."
     new_board_hash = board_hash
     for idx in range(num_pieces):
-        new_board_hash ^= _piece_keys[squares[idx], colored_piece_types[idx]]  # type: ignore
+        new_board_hash ^= _PIECE_KEYS[squares[idx], colored_piece_types[idx]]  # type: ignore
     return new_board_hash
 
 
 @njit(cache=True, nogil=True)
 def _turn_hash(board_hash: np.int64) -> np.int64:
-    return board_hash ^ _turn_key
+    return board_hash ^ _TURN_KEY
 
 
 @njit(cache=True, nogil=True)
 def _conditional_turn_hash(board_hash: np.int64, board_turn: bool) -> np.int64:
-    return board_hash ^ _turn_key if board_turn else board_hash
+    return board_hash ^ _TURN_KEY if board_turn else board_hash
 
 
 @njit(cache=True, nogil=True)
 def _en_passant_hash(board_hash: np.int64, en_passant_file: np.int8) -> np.int64:
     return (  # type: ignore
-        board_hash ^ _en_passant_keys[en_passant_file]
-        if en_passant_file != _int8_max_val
+        board_hash ^ _EN_PASSANT_KEYS[en_passant_file]
+        if en_passant_file != _INT8_MAX_VAL
         else board_hash
     )
 
@@ -72,7 +71,7 @@ def _castling_hash(board_hash: np.int64, castling_rights: np.ndarray) -> np.int6
     for idx in range(num_castle_rights):
         if castling_rights[idx]:
             castling_keys_idx += 1 << idx
-    return board_hash ^ _castling_keys[castling_keys_idx]  # type: ignore
+    return board_hash ^ _CASTLING_KEYS[castling_keys_idx]  # type: ignore
 
 
 @njit(cache=True, nogil=True)
@@ -162,7 +161,7 @@ class ZobristHasher:
         return (
             np.int8(chess.square_file(board.ep_square))  # type: ignore
             if board.ep_square
-            else _int8_max_val
+            else _INT8_MAX_VAL
         )
 
     @staticmethod
