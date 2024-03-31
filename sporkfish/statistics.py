886a33128bac20c5b42223aa10ba6edef7f2ca4e
Jeremy Lo <ccjeremy.lo@gmail.com>
2024-03-31 02:13:24 +0100

    issue 99: updated statistics class to distinguish different nodes and added tests

--- sporkfish/statistics.py
+++ sporkfish/statistics.py
@@ -1,26 +1,84 @@
+from enum import Enum
+from typing import Dict
+
+
+class NodeTypes(Enum):
+    NEGAMAX = "NEGAMAX"
+    NEGAMAX_LAZY_SMP = "NEGAMAX_LAZY_SMP"
+    QUIESCENSE = "QUIESCENSE"
+
+
 class Statistics:
     """A class for tracking statistics related to node visits."""
 
-    def __init__(self, nodes_visited: int):
+    def __init__(self):
         """
         Initialize the Statistics object.
 
         Attributes:
         - nodes_visited (Value): The number of nodes visited. Shared across processes.
         """
-        self.nodes_visited = nodes_visited
+        self._nodes_visited = {
+            NodeTypes.NEGAMAX: 0,
+            NodeTypes.NEGAMAX_LAZY_SMP: 0,
+            NodeTypes.QUIESCENSE: 0
+        }
+        self._pruned = 0
+        self._nodes_in_tt = 0
 
-    def increment(self, count: int = 1) -> None:
+    def increment_node_visited(self, visited_node_type: NodeTypes, count: int = 1) -> None:
         """
         Increment the nodes_visited count.
 
         Parameters:
         - count (int): The number of nodes to increment the count by. Default is 1.
         """
-        self.nodes_visited += count
+        self._nodes_visited[visited_node_type] += count
 
-    def reset(self) -> None:
+    def increment_pruning(self, count: int = 1) -> None:
         """
-        Reset the statistics by setting nodes_visited to 0 and updating start_time to the current time.
+        xxx
         """
-        self.nodes_visited = 0
+        self._pruned += count
+
+    def increment_nodes_in_tt(self, count: int = 1) -> None:
+        """
+        xxx
+        """
+        self._nodes_in_tt += count
+
+    def reset_node_visited(self, default_val = 0) -> None:
+        """
+        Reset the statistics by setting nodes_visited to 0 
+        and updating start_time to the current time. ???
+        """
+        for key in self._nodes_visited.keys():
+            self._nodes_visited[key] = default_val
+
+    def reset_pruning(self, default_val = 0) -> None:
+        self._pruned = default_val
+
+    def reset_nodes_in_tt(self, default_val = 0) -> None:
+        self._nodes_in_tt = default_val
+
+    @property
+    def pruned(self) -> int:
+        """
+        xxx
+        """
+        return self._pruned
+    
+    @property
+    def nodes_in_tt(self) -> int:
+        """
+        xxx
+        """
+        return self._nodes_in_tt
+    
+    @property
+    def nodes_visited(self) -> Dict[NodeTypes, int]:
+        """
+        dda
+        """
+        return self._nodes_visited
+
---
+++ tests/test_statistics.py
@@ -0,0 +1,53 @@
+import pytest
+
+from sporkfish.statistics import Statistics, NodeTypes
+
+class TestIncrementStatistics:
+    def test_increment_node_visited(self):
+        S = Statistics()
+        for k, v in S.nodes_visited.items():
+            assert v == 0
+            S.increment_node_visited(k)
+        
+        for k, v in S.nodes_visited.items():
+            assert v == 1
+
+    def test_increment_pruning(self):
+        S = Statistics()
+        assert S.pruned == 0
+        S.increment_pruning()
+        assert S.pruned == 1
+
+    def test_increment_nodes_in_tt(self):
+        S = Statistics()
+        assert S.nodes_in_tt == 0
+        S.increment_nodes_in_tt()
+        assert S.nodes_in_tt == 1
+
+class TestResetStatistics:
+    def test_reset_node_visited(self):
+        S = Statistics()
+        for k, v in S.nodes_visited.items():
+            assert v == 0
+            S.increment_node_visited(k, 3)
+            assert S.nodes_visited[k] == 3
+        
+        S.reset_node_visited()
+        for k, v in S.nodes_visited.items():
+            assert v == 0
+
+    def test_reset_pruning(self):
+        S = Statistics()
+        assert S.pruned == 0
+        S.increment_pruning(3)
+        assert S.pruned == 3
+        S.reset_pruning(0)
+        assert S.pruned == 0
+
+    def test_reset_nodes_in_tt(self):
+        S = Statistics()
+        assert S.nodes_in_tt == 0
+        S.increment_nodes_in_tt(32)
+        assert S.nodes_in_tt == 32
+        S.reset_nodes_in_tt(0)
+        assert S.nodes_in_tt == 0
